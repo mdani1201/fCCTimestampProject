49ba8e84dd828357c5040dff70cf65570d40352c
mdani1201 M <modani1201@gmail.com>
2024-10-15 06:09:36 +0000

    working with unix and invalid string too

--- index.js
+++ index.js
@@ -67,9 +67,6 @@ app.get(
   }
 );
 
-
-
-
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
