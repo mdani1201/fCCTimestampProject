fa67fd9393f93582e2b1177368566b5d97e1d489
mdani1201 M <modani1201@gmail.com>
2024-10-15 06:50:35 +0000

    rebased everything

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 
 // init project
 var express = require("express");
-var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -12,24 +11,18 @@ var cors = require("cors");
 const { format } = require("express/lib/response");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 // so that your API is remotely testable by FCC
-var cors = require("cors");
-const { format } = require("express/lib/response");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
   res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
   res.json({ greeting: "hello API" });
 });
 
@@ -46,7 +39,6 @@ app.get(
     date = req.params.date;
     var dateValue = Number(date);
     var utcStamp = new Date(dateValue).toUTCString();
-
 
     if (!isNaN(dateValue)) {
       res.json({ unix: dateValue, utc: utcStamp });
@@ -69,6 +61,5 @@ app.get(
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log("Your app is listening on port " + listener.address().port);
   console.log("Your app is listening on port " + listener.address().port);
 });
